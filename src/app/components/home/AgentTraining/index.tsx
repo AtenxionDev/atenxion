757b13ae4d7d837f770f9f1e341a2d549383f589
Chan405 <ayenyeinchanmoe2712@gmail.com>
2024-08-15 10:12:10 +0630

    add img for agent training

--- src/app/components/home/AgentTraining/index.tsx
+++ src/app/components/home/AgentTraining/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import SubHeading from "../../typeface/SubHeading";
 import AgentTrainingAccordion from "../../AgentTrainingAccordion";
 import Layout from "../../common/Layout";
+import Image from "next/image";
 
 export interface PanelData {
   id: string;
@@ -18,41 +19,41 @@ const data = [
     title: "1:  Import your data",
     description:
       "Seamlessly upload your files, such as PDFs, Word documents, add a Website or sync data from platforms like Google Drive and Notion. ",
-    image: "gray",
+    image: "/assets/agent/step1.svg",
   },
   {
     id: "panel1",
     title: "2:  Customize Behavior & Appearance",
     description:
       "Seamlessly upload your files, such as PDFs, Word documents, add a Website or sync data from platforms like Google Drive and Notion. ",
-    image: "lightblue",
+    image: "/assets/agent/step2.svg",
   },
   {
     id: "panel2",
     title: "3:  Define Actions & Conversation Starters",
     description:
       "Seamlessly upload your files, such as PDFs, Word documents, add a Website or sync data from platforms like Google Drive and Notion. ",
-    image: "black",
+    image: "/assets/agent/step3.svg",
   },
   {
     id: "panel3",
     title: "4:  Embed or Integrate with Your Applications",
     description:
       "Seamlessly upload your files, such as PDFs, Word documents, add a Website or sync data from platforms like Google Drive and Notion. ",
-    image: "pink",
+    image: "/assets/agent/step4.svg",
   },
   {
     id: "panel4",
     title: "5:  Monitor Conversations & Gain Insights",
     description:
       "Seamlessly upload your files, such as PDFs, Word documents, add a Website or sync data from platforms like Google Drive and Notion. ",
-    image: "lightCoral",
+    image: "/assets/agent/step5.svg",
   },
 ];
 
 const AgentTraining = () => {
   const [expanded, setExpanded] = React.useState<string | false>("panel0");
-  const [image, setImage] = React.useState("gray");
+  const [image, setImage] = React.useState("/assets/agent/step1.svg");
   const handleChange =
     (panel: PanelData) =>
     (event: React.SyntheticEvent, isExpanded: boolean) => {
@@ -60,6 +61,7 @@ const AgentTraining = () => {
       setExpanded(isExpanded ? panel.id : false);
       setImage(panel.image);
     };
+  console.log(image);
   return (
     <Layout bgColor="custom.cream">
       <Box
@@ -83,15 +85,26 @@ const AgentTraining = () => {
             flexDirection: { xs: "column", md: "row" },
             justifyContent: "center",
             alignItems: "center",
-            gap: "40px",
+            gap: { xs: "60px", md: "40px", lg: "60px" },
+            px: 4,
           }}
         >
-          <Box
+          {/* <Box
             width={{ xs: "100%", md: "50%" }}
             height={{ xs: "300px", md: "500px" }}
-            bgcolor={image}
-          ></Box>
-          <Box width={{ xs: "100%", md: "40%" }}>
+            mb={{ xs: 12, sm: 18, md: 0 }}
+            alignItems={{ xs: "center" }}
+          > */}
+          <Image
+            src={image}
+            alt="steps"
+            width={400}
+            height={450}
+            layout="responsive"
+            style={{ maxWidth: "500px" }}
+          />
+          {/* </Box> */}
+          <Box width={{ xs: "100%", md: "70%", lg: "40%" }}>
             <AgentTrainingAccordion
               data={data}
               handleChange={handleChange}
