a0c2dec2e46535cd8b60f4eda0205216d3965ae6
kyawwunna356 <kyawwunnaaung12345@gmail.com>
2024-08-16 12:09:23 +0700

    added sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,18 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.atenxion.ai/',
+      url: "https://www.atenxion.ai/",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: 'https://www.atenxion.ai/book-a-demo',
+      url: "https://www.atenxion.ai/book-a-demo",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.8,
     },
-  ]
+  ];
 }
